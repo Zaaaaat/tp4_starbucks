dc0ae9c07c6d33200b8a430e292ca03794b2ac46
Steve <112937622+Zaaaaat@users.noreply.github.com>
2023-11-22 15:29:35 +0100

    Question 2

---
+++ src/app/(login)/connexion/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import {z} from 'zod';
+import {useForm, zodResolver} from '@mantine/form';
+import {NumberInput, TextInput, Button, Box, Group, PasswordInput} from '@mantine/core';
+import {User} from "@phosphor-icons/react";
+import Link from "next/link";
+
+const schema = z.object({
+    name: z.string().min(2, {message: 'Name should have at least 2 letters'}),
+    email: z.string().email({message: 'Invalid email'}),
+    password: z.string().min(6, {message: 'Password should have at least 6 letters'}),
+});
+
+export default function Connexion() {
+
+    const form = useForm({
+        validate: zodResolver(schema),
+        initialValues: {
+            name: '',
+            email: '',
+            password: '',
+        },
+    });
+
+    return (
+
+        <Box maw={340} mx="auto"  className="shadow-lg p-7 my-2">
+            CONNEXION
+
+            <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                <TextInput
+                    withAsterisk
+                    label="Name"
+                    placeholder="John Doe"
+                    description="Le nom qui sera utilisé pour vos commandes"
+                    mt="sm"
+                    {...form.getInputProps('name')}
+                />
+                <TextInput
+                    withAsterisk
+                    label="Email"
+                    placeholder="example@mail.com"
+                    {...form.getInputProps('email')}
+                />
+                <PasswordInput
+                    withAsterisk
+                    label="Mot de passe"
+                    placeholder=""
+                    {...form.getInputProps('password')}
+                />
+
+                <Button type="submit" className="bg-green-600 flex justify-center my-5 hover:bg-green-700" fullWith="true">
+                    S'inscrire
+                </Button>
+
+                <Link href="/inscription" className="text-green-700 flex justify-center">
+                    Pas encore de compte? S'inscrire
+                </Link>
+
+            </form>
+        </Box>
+    );
+}
