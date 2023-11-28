676d74d1ae751e78f1d5d359f84889c1de2ab253
Steve <112937622+Zaaaaat@users.noreply.github.com>
2023-11-28 14:05:46 +0100

    Question 2.2

--- src/app/(login)/connexion/page.tsx
+++ src/app/(login)/connexion/page.tsx
@@ -4,10 +4,10 @@ import {z} from 'zod';
 import {useForm, zodResolver} from '@mantine/form';
 import {NumberInput, TextInput, Button, Box, Group, PasswordInput} from '@mantine/core';
 import {User} from "@phosphor-icons/react";
+import {SectionContainer, NoticeMessage} from "tp-kit/components";
 import Link from "next/link";
 
 const schema = z.object({
-    name: z.string().min(2, {message: 'Name should have at least 2 letters'}),
     email: z.string().email({message: 'Invalid email'}),
     password: z.string().min(6, {message: 'Password should have at least 6 letters'}),
 });
@@ -25,18 +25,24 @@ export default function Connexion() {
 
     return (
 
+        <SectionContainer wrapperClassName="max-w-5xl">
         <Box maw={340} mx="auto"  className="shadow-lg p-7 my-2">
-            CONNEXION
+
+            <NoticeMessage
+                message="Votre inscription a bien été prise en compte. Validez votre adresse mail pour vous connecter"
+                onDismiss={function noRefCheck(){}}
+                type="success"
+            />
+
+            <NoticeMessage
+                message="Cette adresse n'est pas disponible !"
+                onDismiss={function noRefCheck(){}}
+                type="error"
+            />
+
+            <h1 className="mb-3">CONNEXION</h1>
 
             <form onSubmit={form.onSubmit((values) => console.log(values))}>
-                <TextInput
-                    withAsterisk
-                    label="Name"
-                    placeholder="John Doe"
-                    description="Le nom qui sera utilisé pour vos commandes"
-                    mt="sm"
-                    {...form.getInputProps('name')}
-                />
                 <TextInput
                     withAsterisk
                     label="Email"
@@ -50,15 +56,16 @@ export default function Connexion() {
                     {...form.getInputProps('password')}
                 />
 
-                <Button type="submit" className="bg-green-600 flex justify-center my-5 hover:bg-green-700" fullWith="true">
+                <Button type="submit" className="bg-green-600 flex justify-center my-5 hover:bg-green-700" fullWidth="true">
                     S'inscrire
                 </Button>
 
-                <Link href="/inscription" className="text-green-700 flex justify-center">
-                    Pas encore de compte? S'inscrire
+                <Link href="/inscription" className="text-green-700 flex justify-center" fullWidth="true">
+                    Créer un compte
                 </Link>
 
             </form>
         </Box>
+        </SectionContainer>
     );
 }
--- src/app/(login)/inscription/page.tsx
+++ src/app/(login)/inscription/page.tsx
@@ -4,7 +4,9 @@ import {z} from 'zod';
 import {useForm, zodResolver} from '@mantine/form';
 import {NumberInput, TextInput, Button, Box, Group, PasswordInput} from '@mantine/core';
 import {User} from "@phosphor-icons/react";
+import {SectionContainer, NoticeMessage} from "tp-kit/components";
 import Link from "next/link";
+import {useState} from "react";
 
 const schema = z.object({
     name: z.string().min(2, {message: 'Name should have at least 2 letters'}),
@@ -23,12 +25,35 @@ export default function Inscription() {
         },
     });
 
+    const [notices, setNotices] = useState([]);
+
+    function addError(message) {
+        setNotices([...notices, { type: "error", message }]);
+    }
+
+    function addSuccess(message) {
+        setNotices([...notices, { type: "success", message }]);
+    }
+
     return (
 
+        <SectionContainer wrapperClassName="max-w-5xl">
         <Box maw={340} mx="auto" className="shadow-lg p-7 my-2">
-            INSCRIPTION
 
-            <form onSubmit={form.onSubmit((values) => console.log(values))}>
+            {notices.map((notice, i) => (
+                <NoticeMessage key={i}{...notice}/>
+            ))}
+
+            <h1 className="mb-3">CONNEXION</h1>
+
+            <form onSubmit={form.onSubmit((values) => {
+                if (values.name === 'errorCondition') {
+                    addError("Cette adresse n'est pas disponible");
+                } else {
+                    addSuccess("Votre inscription a bien été prise en compte. Validez votre adresse mail pour vous connecter");
+                }
+            })}>
+
                 <TextInput
                     withAsterisk
                     label="Name"
@@ -50,7 +75,7 @@ export default function Inscription() {
                     {...form.getInputProps('password')}
                 />
 
-                <Button type="submit" className="bg-green-600 flex justify-center my-5 hover:bg-green-700" fullWith="true">
+                <Button type="submit" className="bg-green-600 flex justify-center my-5 hover:bg-green-700" fullWidth="true">
                     S'inscrire
                 </Button>
 
@@ -59,5 +84,6 @@ export default function Inscription() {
                 </Link>
             </form>
         </Box>
+        </SectionContainer>
     );
 }
