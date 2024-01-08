9704dc914c3725def5c118eda7d7f0814ec0cf81
Steve <112937622+Zaaaaat@users.noreply.github.com>
2024-01-08 11:56:20 +0100

    Question 2.10

--- src/actions/create-order.ts
+++ src/actions/create-order.ts
@@ -3,10 +3,21 @@
 import { computeCartTotal, computeLineSubtotal } from "../hooks/use-cart";
 import { CartData } from "../types";
 import prisma from "../utils/prisma";
+import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
+import {cookies} from "next/headers";
 
 export async function createOrder(cart: CartData) {
+  const supabase = createServerComponentClient({ cookies });
+  const {data} = await supabase.auth.getUser();
+  const userId = data.user?.id;
+
+  if (!userId) {
+    console.log("User not logged in");
+    }
+
   console.log(await prisma.order.create({
     data: {
+      userid: userId,
       total: computeCartTotal(cart.lines),
       lines: {
         create: cart.lines.map(line => ({
